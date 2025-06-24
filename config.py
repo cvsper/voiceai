f727c84704fba8aa78fa38d396c1c074c2bd0460
shamar donaldson <sevs@macbookpro.mynetworksettings.com>
2025-06-24 10:58:17 -0400

    voice agent

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///voiceai.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROVIDE_AUTOMATIC_OPTIONS = False
     
     # Twilio Configuration
     TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
